0d89ddc06bcd4cf8a57c056377f368875f7c8dd0
Clash2453 <spasmilenkov123@gmail.com>
2022-07-12 21:42:50 +0300

    Add function to return keys to original state

--- js/script.js
+++ js/script.js
@@ -1,7 +1,18 @@
+
 window.addEventListener('keydown', function(e){
     const player = document.querySelector(`audio[data-key="${e.code}"]`);
+    const keyStroke = document.querySelector(`.key[data-key="${e.code}"]`);
+    if(!keyStroke) return;
+    keyStroke.classList.add('active');
     if(!player)
         return;
     player.currentTime = 0;
     player.play();
 })
+function removeTransition(e){
+    if(e.propertyName !== 'transform') return;
+    console.log(e.propertyName);
+    this.classList.remove('active')
+}
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
