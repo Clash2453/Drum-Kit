7f2de31fbf5d9212715f8c96291ec399a9993f72
Clash2453 <spasmilenkov123@gmail.com>
2022-07-12 21:49:47 +0300

    Separated the logic into different functions

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
-
-window.addEventListener('keydown', function(e){
+function playSound(e){ //Add event to listen for the correct keypress to play a sound
     const player = document.querySelector(`audio[data-key="${e.code}"]`);
     const keyStroke = document.querySelector(`.key[data-key="${e.code}"]`);
     if(!keyStroke) return;
@@ -8,11 +7,14 @@ window.addEventListener('keydown', function(e){
         return;
     player.currentTime = 0;
     player.play();
-})
-function removeTransition(e){
+}
+
+window.addEventListener('keydown', playSound);
+
+function removeTransition(e){ //Reverts the .key to its original state
     if(e.propertyName !== 'transform') return;
     console.log(e.propertyName);
     this.classList.remove('active')
 }
 const keys = document.querySelectorAll('.key');
-keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));//loop through all keys and to subscribe them for removeTransition
